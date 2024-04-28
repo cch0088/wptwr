7c57397a8d2cabe22d7bac18d747de981dabea07
cch0088 <cons1@protonmail.ch>
2024-04-27 20:51:32 -0400

    Delete me

--- react-src/src/components/user/Profile.jsx
+++ react-src/src/components/user/Profile.jsx
@@ -1,28 +1,10 @@
 import React from "react";
-import { useMutation } from '@apollo/client';
-import { getRoot } from '../../lib/validation';
-import { useNavigate } from 'react-router-dom';
-import { WP_ROOT } from '../../config';
-import { DELETE_USER } from '../../gql';
 import { useSelector } from "react-redux";
 
 export default function Profile() {
+const user = useSelector(state => state.user.value);
 
-    const navigate = useNavigate();
-    const user = useSelector(state => state.user.value);
-
-    const [deleteUser, { loading }] = useMutation(DELETE_USER);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        deleteUser({
-            variables: {
-                id: user.id
-            },
-            })
-        .then((_status) => navigate(getRoot(WP_ROOT)));
-    }
+//TODO: Insert link to delete me plugin
 
 return (
         <div>
@@ -31,8 +13,6 @@ return (
                 type="submit"
                 name="delete"
                 value="Delete Me"
-                onClick={(e) => { handleSubmit(e); }}
-                disabled={loading}
             />
         </div>
     )
